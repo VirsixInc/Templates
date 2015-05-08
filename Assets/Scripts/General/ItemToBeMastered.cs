cf30fa34513903e130e6aa549ba67f5f8729e4b7
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-08 10:21:56 -0700

    added generic itemtobemastered class

---
+++ Assets/Scripts/General/ItemToBeMastered.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemToBeMastered {
+
+	public float sequenceMastery = 0; //incremented by .25, used to adjust difficulty
+	public GameObject itemGameObject;
+	public ItemToBeMastered (float mastery, GameObject newGameObject){
+		sequenceMastery = mastery;
+		itemGameObject = newGameObject;
+	}
+}
