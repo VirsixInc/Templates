f126e8bae1f03bfe15126b01de933d2ba7f59e88
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-11 11:56:34 -0700

    randomization on elements and event trigger script

---
+++ Assets/HotSpotClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger))]
+public class HotSpotClick : MonoBehaviour {
+
+	EventTrigger thisEventTrigger;
+	// Use this for initialization
+	void Start () {
+		thisEventTrigger = GetComponentInParent<EventTrigger>();
+		EventTrigger.Entry entry = new EventTrigger.Entry();
+		entry.eventID = EventTriggerType.PointerDown;
+		entry.callback.AddListener( (eventData) => { HotSpotsGame.s_instance.SubmitAnswer(gameObject.name); } );
+		thisEventTrigger.delegates.Add(entry);
+	}
+	
+	//HotSpotsGame.s_instance.SubmitAnswer(gameObject.name)
+	void Update () {
+	
+	}
+}
