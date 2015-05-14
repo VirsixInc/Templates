32cb6b9202f89af0b6509524717303bd99dbd563
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-14 12:03:34 -0700

    assignmentManager updae

--- Assets/Scripts/General/MainMenu/AssignmentManager.cs
+++ Assets/Scripts/General/MainMenu/AssignmentManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class AssignmentManager : MonoBehaviour {
 	public List<GameObject> completedAssignments, incompleteAssignments;
 	public Assignment assignment;
-	public GameObject masteredHeader, assignmentsHeader;
+	public GameObject masteredHeader, unmasteredHeader;
 	public GameObject assignmentGUIPrefab;
 	float prefabHeight = 200f, initPrefabYPos = -50f, leftPos = -300f, rightPos = 300f, dividerHeight = 225f, spaceBetweenAssignments = 100f;
 	public float upperBound; //for clamping scroll
@@ -18,40 +18,31 @@ public class AssignmentManager : MonoBehaviour {
 	This functionality occurs in the Main Menu scene.
 	
 	At start, Assignment manager looks through app manager and gets the list of the assignments
-	separate them into completed and incomplete assignments.
+	separate them into completed and incomplete assignments. Set each of these to be childed to
+	either the masteredHeader or the unmasteredHeader, local positions will then be set.
 
-	the count of type assignment will be divided by two and rounded up
-	loop through and instantiate each 
-
-	instantiate each even prefab on the left at -150 
-
-	*/
+	 */
 
 
 	void Awake () {
 		s_instance = this;
 	}
-
-	void LoadAssignment() {
-		//	foldername.csv, foldername.json, foldername.spritesheet
-		// 
-	}
-
-
+	
 	// Use this for initialization
-	public void LoadAllAssignments(Assignment[] arrayOfAssignments){
+	public void LoadAllAssignments(List<Assignment> arrayOfAssignments){
 		s_instance = this;
+
+		//parse associatedGameObjects into either mastered or unmastered
 		foreach (Assignment assignment in arrayOfAssignments) {
 			if (assignment.isCompleted) {
-				completedAssignments.Add(assignment.associatedGUIObject); //this is currently being called when it should not
-				assignment.associatedGUIObject.transform.parent = assignmentsHeader.transform;
+				completedAssignments.Add(assignment.associatedGUIObject);
+				assignment.associatedGUIObject.transform.parent = unmasteredHeader.transform;
 			}
 			else {
 				incompleteAssignments.Add(assignment.associatedGUIObject);
 				assignment.associatedGUIObject.transform.parent = masteredHeader.transform;
 			}
 		}
-
 		//calculate upper bound based off amount of assignments
 		if (incompleteAssignments.Count%2 == 0 && completedAssignments.Count%2 == 0)
 			upperBound = AppManager.s_instance.userAssignments.Count * (prefabHeight + 2 * spaceBetweenAssignments + dividerHeight) / 2;
@@ -79,12 +70,6 @@ public class AssignmentManager : MonoBehaviour {
 			incompleteAssignments[i].transform.localPosition = assignmentPosition;
 		}
 
-		float completedAssignmentOffset;
-		if (incompleteAssignments.Count % 2 == 0)
-			completedAssignmentOffset = -incompleteAssignments.Count * (spaceBetweenAssignments + prefabHeight) - dividerHeight;
-		else
-			completedAssignmentOffset = -(incompleteAssignments.Count+1) * (spaceBetweenAssignments + prefabHeight) - dividerHeight;
-
 		for (int i = 0; i < completedAssignments.Count; i++) {
 			//if i is even, put it in left column
 			if (i%2 == 0){
@@ -97,12 +82,18 @@ public class AssignmentManager : MonoBehaviour {
 			completedAssignments[i].transform.localPosition = assignmentPosition; //local scale selects canvas space instead of w space
 		}
 
+		float completedAssignmentOffset;
+		if (incompleteAssignments.Count % 2 == 0)
+			completedAssignmentOffset = -incompleteAssignments.Count * (spaceBetweenAssignments + prefabHeight) - dividerHeight;
+		else
+			completedAssignmentOffset = -(incompleteAssignments.Count+1) * (spaceBetweenAssignments + prefabHeight) - dividerHeight;
+
 		//set divider position for the second 
 		if (completedAssignments.Count > 0)
 			masteredHeader.transform.localPosition = new Vector3 (0, completedAssignmentOffset + 125, 0);
 		else
 			masteredHeader.gameObject.SetActive (false);
-		assignmentsHeader.transform.localPosition = new Vector3 (0, initPrefabYPos + 350f, 0);
+		unmasteredHeader.transform.localPosition = new Vector3 (0, initPrefabYPos + 350f, 0);
 	}
 
 }
