945069c323f3db41015d026b2ccdea29cc53082f
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-16 10:19:11 -0700

    refactored fader.cs

---
+++ Assets/AnimationSlide.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSlide : MonoBehaviour {
+
+	//this is for menu GUI items to look extra cool when they pop up
+
+	float lerpTime = 0.5f;
+	float currentLerpTime;
+	
+	float moveDistance = 10f;
+	
+	Vector3 startPos, endPos, originalPos;
+
+	void Awake () {
+		originalPos = transform.position;
+	}
+	
+	protected void Start() {
+		startPos = originalPos;
+		endPos = transform.position + transform.up * moveDistance;
+	}
+	
+	protected void Update() {
+		//reset when we press spacebar
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			currentLerpTime = 0f;
+		}
+		
+		//increment timer once per frame
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+		
+		//lerp!
+		float perc = currentLerpTime / lerpTime;
+		transform.position = Vector3.Lerp(startPos, endPos, perc);
+	}
+}
---
+++ Assets/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+	Fader fader;
+
+	void Awake(){
+		fader = GetComponent<Fader> ();
+	}
+
+	public void ActivateMenuButtons () {
+
+	}
+}
