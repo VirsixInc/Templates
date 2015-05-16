af273e69505836cb594f3be97ed65bab8533b4f8
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-16 11:51:04 -0700

    menubuttonmanager

--- Assets/AnimationSlide.cs
+++ Assets/AnimationSlide.cs
@@ -1,40 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum SlideDirection {Up, Down, Left, Right}
 public class AnimationSlide : MonoBehaviour {
 
 	//this is for menu GUI items to look extra cool when they pop up
+	public float startTime, fadeTime = 1;
 
-	float lerpTime = 0.5f;
-	float currentLerpTime;
-	
 	float moveDistance = 10f;
-	
-	Vector3 startPos, endPos, originalPos;
+	public SlideDirection thisSlideDirection = SlideDirection.Up;
+	Vector3 startPos, endPos, originalPos, moveDirection;
+	bool isSliding = false;
 
 	void Awake () {
 		originalPos = transform.position;
-	}
-	
-	protected void Start() {
+		switch (thisSlideDirection) {
+		case SlideDirection.Down :
+			moveDirection = -transform.up;
+			break;
+
+		case SlideDirection.Up :
+			moveDirection = transform.up;
+			break;
+
+		case SlideDirection.Left :
+			moveDirection = -transform.right;
+			break;
+
+		case SlideDirection.Right :
+			moveDirection = transform.right;
+			break;
+		}
 		startPos = originalPos;
-		endPos = transform.position + transform.up * moveDistance;
+		endPos = transform.position + moveDirection * moveDistance;
 	}
 	
-	protected void Update() {
-		//reset when we press spacebar
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			currentLerpTime = 0f;
-		}
-		
+	public void Slide() {
+		transform.position = originalPos;
+		isSliding = true;
+		startTime = Time.time;
+	}
+	
+	void Update() {
+		float timePassed = (Time.time - startTime);
+		float fracJourney = timePassed / fadeTime;
 		//increment timer once per frame
-		currentLerpTime += Time.deltaTime;
-		if (currentLerpTime > lerpTime) {
-			currentLerpTime = lerpTime;
+
+		transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
+		if (fracJourney >= 1) {
+			isSliding = false;
 		}
-		
-		//lerp!
-		float perc = currentLerpTime / lerpTime;
-		transform.position = Vector3.Lerp(startPos, endPos, perc);
 	}
 }
---
+++ Assets/MenuButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonManager : MonoBehaviour {
+
+	//this class handles the triggering of menu button events
+	Fader[] faders;
+	AnimationSlide[] animationSlides;
+	void Awake(){
+		faders = GetComponentsInChildren<Fader> ();
+		animationSlides = GetComponentsInChildren<AnimationSlide> ();
+	}
+	
+	void OnEnable() {
+		ActivateMenuButtons ();
+	}
+	
+	public void ActivateMenuButtons () {
+		if (faders != null) {
+			foreach (Fader f in faders) {
+				f.StartFadeOut ();
+			}
+		}
+		if (animationSlides != null) {
+			//			animationSlide.s ();
+		}
+	}
+	
+	public void DeActivateMenuButtons () {
+		if (faders != null) {
+			foreach (Fader f in faders) {
+				f.StartFadeIn ();
+			}
+		}
+	}
+}
