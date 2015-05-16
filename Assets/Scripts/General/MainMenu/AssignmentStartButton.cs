afbd2150f0c8e2c9645d99d4b95adb0eb0cddaa4
Pablo Leon-Luna <paleonluna@gmail.com>
2015-05-16 14:06:20 -0700

    fixed event handler in the star button

--- Assets/Scripts/General/MainMenu/AssignmentStartButton.cs
+++ Assets/Scripts/General/MainMenu/AssignmentStartButton.cs
@@ -13,12 +13,13 @@ public class AssignmentStartButton : MonoBehaviour {
 	void Start () {
 		thisEventTrigger = GetComponent<EventTrigger>();
 		entry.eventID = EventTriggerType.PointerDown;
-		entry.callback.AddListener( (eventData) => {OnClick(); } );
+		entry.callback.AddListener( (eventData) => {CallManager(); } );
 		thisEventTrigger.delegates.Add(entry);
 	}
 
-	void OnClick() {
-		SoundManager.s_instance.PlaySound (SoundManager.s_instance.m_start);
+	void CallManager() {
+		print ("CallManager called");
+//		SoundManager.s_instance.PlaySound (SoundManager.s_instance.m_start);
 		AppManager.s_instance.ClickHandler(transform.parent.gameObject.GetComponent<AssignmentGUI>().assignmentIndex);
 		//AppManager.s_instance.currentAssignment = transform.parent.GetComponent<Assignment> ();
 //		AppManager.s_instance.currentAppState = AppState.Playing;
