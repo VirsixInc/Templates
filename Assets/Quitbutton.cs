3dc359bfa7f83bb9e1a995070b4970cc480ab948
paleonluna <paleonluna@gmail.com>
2015-05-26 11:44:29 -0700

    quit button

--- Assets/Quitbutton.cs
+++ Assets/Quitbutton.cs
@@ -7,8 +7,9 @@ public class Quitbutton : MonoBehaviour {
 	public Slider mastery;
 
 	public void SaveAndQuit () {
-//		int masteryOutput = Mathf.CeilToInt (mastery.value * 100);
-//		AppManager.s_instance.saveAssignmentMastery (AppManager.s_instance.currentAssignments [AppManager.s_instance.currIndex], masteryOutput);
+		//			int masteryOutput = Mathf.CeilToInt(mastery.value*100);
+		AppManager.s_instance.uploadAssignMastery(AppManager.s_instance.currentAssignments[AppManager.s_instance.currIndex].assignmentTitle, masteryOutput);
+		//		int masteryOutput = Mathf.CeilToInt (mastery.value * 100);
+		//		AppManager.s_instance.saveAssignmentMastery (AppManager.s_instance.currentAssignments [AppManager.s_instance.currIndex], masteryOutput);
 		Application.LoadLevel ("AssignmentMenu");
-	}
 }
